bf8ccddf37e37abf8c62a9bd34c3c6ec8a98b351
Muhammed Furkan <mfyilmaz.hukuk@gmail.com>
2023-01-22 00:12:59 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider, useSelector } from "react-redux";
+import DarkModeSwitcher from "./components/dark-mode/dark-mode-switcher";
+import LangSwitcher from "./components/lang-switcher/lang-switcher";
+import store from "./store";
+import "./App.css";
+import { $t } from "./helpers/locale-helper";
 
-function App() {
+const App = () => {
+  const darkMode = useSelector((state) => state.theme.darkMode);
+
+  // Tüm APP i re-render etmek için yazdık. aslında burada curretnLang e ihtiyacımız yok
+  // Merkezi state teki lang değişince useSelectorden dolayı APP componenti re-render olacak
+  const currentLang = useSelector((state) => state.locale.lang);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "dark" : ""}>
+      <DarkModeSwitcher />
+      <LangSwitcher />
+
+      <h1>{$t("hello-world")}</h1>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "flag-icons/css/flag-icons.min.css";
+import { Provider as StoreProvider } from "react-redux";
+import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
   </React.StrictMode>
 );
 
